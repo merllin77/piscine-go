ddf5dc6866b16de61f8a5f0dedb704bd6b6da8c1
Konstantinos Tallaris <ktallaris@noreply.platform.zone01.gr>
2025-11-03 20:38:35 +0000

    Update to-git-or-not-to-git.sh

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,3 +1,3 @@
-# !/bin/bash
-curl -o superhero https://platform.zone01.gr/assets/superhero/all.json
+#!/bin/bash
+curl -s -k "https://platform.zone01.gr/assets/superhero/all.json" -o superhero
 jq -r '.[] | select(.id == 170) | .name, .powerstats.power, .appearance.gender' superhero
