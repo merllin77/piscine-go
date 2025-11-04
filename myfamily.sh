67985df87af20601d8a9a8f834ccc69ae5cde1ed
Konstantinos Tallaris <ktallaris@noreply.platform.zone01.gr>
2025-11-04 10:37:13 +0000

    Update myfamily.sh

--- myfamily.sh
+++ myfamily.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl -s https://platform.zone01.gr/assets/superhero/all.json \
 | jq -r --arg family_id "$HERO_ID" '.[] | select(.id == ($family_id | tonumber)) | .connections.relatives 
-| gsub("\n"; " ") 
+| gsub("\\r?\\n"; " ")
 | gsub("\\s+"; " ")
 | gsub("^\\s+|\\s+$"; "")'
