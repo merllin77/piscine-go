4e4b30c55b0b0b062585b7c417251419333a8625
ktallaris <netcitizen@email.com>
2025-11-06 14:38:59 +0200

    updated

--- printcomb.go
+++ printcomb.go
@@ -9,8 +9,13 @@ func PrintComb() {
 				z01.PrintRune(i)
 				z01.PrintRune(j)
 				z01.PrintRune(k)
-				z01.PrintRune(',')
+				if !(i == '7' && j == '8' && k == '9') {
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				}
+
 			}
 		}
 	}
+	z01.PrintRune('\n')
 }
