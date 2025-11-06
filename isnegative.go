2ac0e51bc0ad65a76786fc37f7de690cbb8ddbe4
ktallaris <netcitizen@email.com>
2025-11-06 12:36:13 +0200

    correct update

--- isnegative.go
+++ isnegative.go
@@ -1,11 +1,12 @@
-package main
+package student
 
-import (
-	"piscine/piscine"
-)
+import "github.com/01-edu/z01"
 
-func main() {
-	piscine.IsNegative(1)
-	piscine.IsNegative(0)
-	piscine.IsNegative(-1)
+func IsNegative(nb int) {
+	if nb < 0 {
+		z01.PrintRune('T')
+	} else {
+		z01.PrintRune('F')
+	}
+	z01.PrintRune('\n')
 }
