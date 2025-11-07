6a286c1e6b8d2c394d2ba7088150f48595738ea5
ktallaris <netcitizen@email.com>
2025-11-07 09:34:50 +0200

    upload

---
+++ pointone.go
@@ -0,0 +1,15 @@
+package student
+
+import "fmt"
+
+func PointOne(n *int){
+	*n=1
+}
+
+/*func main(){
+	newvalue:=0
+	fmt.Println(newvalue, ", ")
+	PointOne(&newvalue)
+	fmt.Println(newvalue)
+
+}*/
