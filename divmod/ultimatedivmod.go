7d42253b25dd6db225bd79913c283e4b41aeb93f
ktallaris <netcitizen@email.com>
2025-11-07 16:10:42 +0200

    upload

---
+++ divmod/ultimatedivmod.go
@@ -0,0 +1,15 @@
+package main
+
+var div int
+var mod int
+var da int
+var db int
+
+func UltimateDivMod(a *int, b *int) {
+	da = *a
+	db = *b
+	div = da / db
+	mod = da % db
+	*a = div
+	*b = mod
+}
