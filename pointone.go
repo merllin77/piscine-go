b4d564f8a29d48b1740161272dca3c30c4f39e94
ktallaris <netcitizen@email.com>
2025-11-07 09:37:36 +0200

    upload

--- pointone.go
+++ pointone.go
@@ -1,15 +1,5 @@
 package student
 
-import "fmt"
-
 func PointOne(n *int){
 	*n=1
 }
-
-/*func main(){
-	newvalue:=0
-	fmt.Println(newvalue, ", ")
-	PointOne(&newvalue)
-	fmt.Println(newvalue)
-
-}*/
