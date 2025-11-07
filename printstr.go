ed8f752846a4022984080ad4bc72bc822529f9b6
ktallaris <netcitizen@email.com>
2025-11-07 17:17:24 +0200

    upload

---
+++ printstr.go
@@ -0,0 +1,11 @@
+package student
+
+import "github.com/01-edu/z01"
+
+func PrintStr(s string) {
+	r := []rune(s)
+	for i := 0; i <= len(r)-1; i++ {
+		z01.PrintRune(r[i])
+	}
+	z01.PrintRune('\n')
+}
