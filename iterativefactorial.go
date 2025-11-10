d8ebb1994b647f9f4b35ffd95f2938200fee5b66
ktallaris <netcitizen@email.com>
2025-11-10 20:29:47 +0200

    updated

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,17 +2,16 @@ package student
 
 func IterativeFactorial(nb int) int {
 	rslt := 1
-	if nb == 0 {
-		return 1
-	} else if nb > 0 {
-		for i := 1; i <= nb; i++ {
-			if rslt > 0 && rslt > 1<<63/i {
-				return 0
-			}
-			rslt = rslt * i
-		}
-		return rslt
-	} else {
+	if nb < 0 {
 		return 0
 	}
+
+	if nb == 0 {
+		return 1
+	}
+
+	for i := 1; i <= nb; i++ {
+		rslt = rslt * i
+	}
+	return rslt
 }
