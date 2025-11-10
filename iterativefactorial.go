04668baeccba69671cd6ca368a85f3ad3c1e34a4
ktallaris <netcitizen@email.com>
2025-11-10 19:25:48 +0200

    upload

---
+++ iterativefactorial.go
@@ -0,0 +1,15 @@
+package main
+
+func IterativeFactorial(nb int) int {
+
+	if nb == 0 {
+		return 1
+	} else if nb > 0 {
+		for i := nb - 1; i >= 1; i-- {
+			nb = nb * i
+		}
+		return nb
+	} else {
+		return 0
+	}
+}
