a6d192a671dd05cdfd8297fb063dbaee7d9b7709
ktallaris <netcitizen@email.com>
2025-11-10 23:08:23 +0200

    upload

---
+++ iterativepower.go
@@ -0,0 +1,15 @@
+package student
+
+func IterativePower(nb int, power int) int {
+	if nb < 0 || nb > 20 || power < 0 || power > 20 {
+		return 0
+	}
+	if nb == 0 {
+		return 1
+	}
+	rslt := 1
+	for i := 1; i <= power; i++ {
+		rslt = rslt * nb
+	}
+	return rslt
+}
