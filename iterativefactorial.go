4c914a6d2a8ea9619e34fa03f98ede19437db8b4
ktallaris <netcitizen@email.com>
2025-11-10 20:13:18 +0200

    updated

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -6,6 +6,9 @@ func IterativeFactorial(nb int) int {
 		return 1
 	} else if nb > 0 {
 		for i := 1; i <= nb; i++ {
+			if result > 0 && result > 1<<63/i {
+				return 0
+			}
 			rslt = rslt * i
 		}
 		return rslt
