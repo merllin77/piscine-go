0dd164219a828e8302885c90fc28ec84ec67ad46
ktallaris <netcitizen@email.com>
2025-11-11 13:48:55 +0200

    upload

--- isprime.go
+++ isprime.go
@@ -4,9 +4,16 @@ func IsPrime(nb int) bool {
 	if nb <= 1 {
 		return false
 	}
-	if nb%2 == 1 {
+	if nb == 2 {
 		return true
-	} else {
+	}
+	if nb%2 == 0 {
 		return false
 	}
+	for i := 3; i*i <= nb; i = i + 2 {
+		if nb%i == 0 {
+			return false
+		}
+	}
+	return true
 }
