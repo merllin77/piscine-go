c723817d47ebaa75aa9c29b2f23734a92e878cea
ktallaris <netcitizen@email.com>
2025-11-12 19:21:47 +0200

    updated

--- isalpha.go
+++ isalpha.go
@@ -1,19 +1,14 @@
-package main
-
-import "fmt"
+package student
 
 func IsAlpha(s string) bool {
+
 	for _, r := range s {
-		if (r < '0' && r > '9') || r < 'A' {
+		if r == ' ' {
+			return true
+		}
+		if r < '0' {
 			return false
 		}
 	}
 	return true
-}
-
-func main() {
-	fmt.Println(IsAlpha("Hello! How are you?"))
-	fmt.Println(IsAlpha("HelloHowareyou"))
-	fmt.Println(IsAlpha("What's this 4?"))
-	fmt.Println(IsAlpha("Whatsthis4"))
 }
