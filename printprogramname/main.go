8d378be859ff1185448ba71bd9e2546fb1d4be40
ktallaris <netcitizen@email.com>
2025-11-14 08:33:12 +0200

    updated

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -9,14 +9,13 @@ import (
 func main() {
 	progname := os.Args[0]
 	slash := 0
-	for i := 0; i < len(progname); i++ {
-		if progname[i] == '/' {
+	for i, r := range progname {
+		if r == '/' {
 			slash = i
 		}
 	}
-	for i := slash + 1; i < len(progname); i++ {
-		z01.PrintRune(rune(progname[i]))
+	for _, r := range progname[slash+1:] {
+		z01.PrintRune(r)
 	}
-
 	z01.PrintRune('\n')
 }
