be6c99c652a32e5e69e7a4c441eb6c55dd326b5c
ktallaris <netcitizen@email.com>
2025-11-14 07:41:55 +0200

    update

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"os"
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
-	progname := os.Args
-	fmt.Println(progname[0])
+	progname := os.Args[0]
+	for _, r := range progname {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
 }
