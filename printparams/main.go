477450b6e2c791fad60c88d9c9b349eb3520b914
ktallaris <netcitizen@email.com>
2025-11-14 09:27:01 +0200

    upload

---
+++ printparams/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	arg := os.Args
+	for _, r := range arg[1:] {
+		for _, j := range r {
+			z01.PrintRune(j)
+		}
+		z01.PrintRune('\n')
+	}
+	z01.PrintRune('\n')
+}
