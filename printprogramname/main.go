8a2cd44533f8b740044440f1929327e2bfaf71a9
ktallaris <netcitizen@email.com>
2025-11-14 08:16:17 +0200

    updated

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"os"
-	"path/filepath"
 
 	"github.com/01-edu/z01"
 )
 
 func main() {
-	progname := filepath.Base(os.Args[0])
+	progname := os.Args[0]
+	slash := 0
 	for i := 0; i < len(progname); i++ {
+		if progname[i] == '/' {
+			slash = i
+		}
+	}
+	for i := slash + 1; i < len(progname); i++ {
 		z01.PrintRune(rune(progname[i]))
 	}
+
 	z01.PrintRune('\n')
 }
