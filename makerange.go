bb48f39fa1db60239c683f3cb4f92fe32a63c939
ktallaris <netcitizen@email.com>
2025-11-14 11:33:29 +0200

    upload

---
+++ makerange.go
@@ -0,0 +1,12 @@
+package main
+
+func MakeRange(min, max int) []int {
+	if min >= max {
+		return nil
+	}
+	ar := []int{}
+	for i := 0; i < (max - min); i++ {
+		ar = append(ar, min+i)
+	}
+	return ar
+}
