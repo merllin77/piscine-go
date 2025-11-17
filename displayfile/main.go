2bbc0f7e1043cbe04d4823c4b515bbe48da80f8e
ktallaris <netcitizen@email.com>
2025-11-17 18:28:04 +0200

    updated

--- displayfile/main.go
+++ displayfile/main.go
@@ -1,32 +1,33 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
 	args := os.Args[1:]
-	line := ""
 
 	if len(args) == 0 {
 		fmt.Println("File name missing")
+		return
 	}
 	if len(args) > 1 {
 		fmt.Println("Too many arguments")
-	}
-
-	content, err := os.Open(args[0]) // open the file (contenct has the file's data in bytes)
-	if err != nil {                  // err == nil means there is no error.
-		fmt.Println("Error opening file")
 		return
 	}
 
-	scanner := bufio.NewScanner(content) // create a "scanner" to read lines
-	for scanner.Scan() {                 // moves to the next line and returns true while there are lines left.
-		line = scanner.Text() // get current line as string
+	file, err := os.Open(args[0]) // open the file (contenct has the file's data in bytes)
+	if err != nil {               // err == nil means there is no error.
+		fmt.Println("Error opening file")
+		return
 	}
-	fmt.Println(line)
-	defer content.Close()
+	defer file.Close() // closing the file after function ends (Important!)
+
+	content, err := io.ReadAll(file) // content has the file's content in bytes
+	if err != nil {
+		fmt.Println("Error reading files", err)
+	}
+	fmt.Println(string(content)) // transpose bytes to string for output
 }
