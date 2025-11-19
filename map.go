696620221583d842febd285c72fd5adc301f0f1e
ktallaris <netcitizen@email.com>
2025-11-19 15:30:56 +0200

    updated

--- map.go
+++ map.go
@@ -1,19 +1,9 @@
 package student
 
 func Map(f func(int) bool, a []int) []bool {
-	rsltbool := []bool{}
-	for _, r := range a {
-		if f(r) == true {
-			rsltbool = append(rsltbool, true)
-		} else {
-			rsltbool = append(rsltbool, false)
-		}
+	rsltbool := make([]bool, len(a))
+	for i, r := range a {
+		rsltbool[i] = f(r)
 	}
 	return rsltbool
 }
-
-/*func main() {
-	a := []int{1, 2, 3, 4, 5, 6, 13}
-	result := Map(IsPrime, a)
-	println(result)
-} */
