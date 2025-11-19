fec1e224f27a7c40d22783bb15acdcb48131316d
ktallaris <netcitizen@email.com>
2025-11-19 15:25:15 +0200

    updated

--- map.go
+++ map.go
@@ -1,28 +1,5 @@
 package student
 
-import "fmt"
-
-/*func IsPrime(nbr int) bool {
-	if nbr <= 1 { // 1 is not a Prime
-		return false
-	}
-
-	if nbr == 2 { // 2 is the first Prime number
-		return true
-	}
-
-	if nbr%2 == 0 { // If modulo == 0 is NOT a Prime number
-		return false
-	}
-
-	for i := 3; i*i <= nbr; i = i + 2 { // check after number 2 which is the first Prime number
-		if nbr%i == 0 { // if nbr is even return TRUE
-			return false
-		}
-	}
-	return true
-} // End of IsPrime */
-
 func Map(f func(int) bool, a []int) []bool {
 	rsltbool := []bool{}
 	for _, r := range a {
@@ -35,8 +12,8 @@ func Map(f func(int) bool, a []int) []bool {
 	return rsltbool
 }
 
-func main() {
+/*func main() {
 	a := []int{1, 2, 3, 4, 5, 6, 13}
 	result := Map(IsPrime, a)
-	fmt.Println(result)
-}
+	println(result)
+} */
