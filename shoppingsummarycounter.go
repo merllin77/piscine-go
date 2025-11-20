4d4739d3081d06625717a0006c2cfcfdfb969016
ktallaris <netcitizen@email.com>
2025-11-20 16:52:15 +0200

    upload

---
+++ shoppingsummarycounter.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func ShoppingSummaryCounter(str string) map[string]int {
+	summary := make(map[string]int)
+	word := ""
+
+	for _, r := range str {
+		if r != ' ' {
+			word = word + string(r)
+		} else if word != "" {
+			summary[word]++
+			word = ""
+		}
+	}
+	if word != "" {
+		summary[word]++
+	}
+	return summary
+}
+
+func main() {
+	summary := "Burger Water Carrot Coffee Water Water Chips Carrot Carrot Burger Carrot Water"
+	for index, element := range ShoppingSummaryCounter(summary) {
+		fmt.Println(index, "=>", element)
+	}
+}
