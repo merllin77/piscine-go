f57d38b3c7550f99924adfa9c06b995c4cc8b39d
ktallaris <netcitizen@email.com>
2025-11-20 17:33:33 +0200

    upload

--- compact.go
+++ compact.go
@@ -1,13 +1,10 @@
-package main
-
-import "fmt"
+package student
 
 // Compact removes zero-value strings from the slice and returns the number of non-zero elements
 func Compact(slice *[]string) int {
 	if slice == nil || len(*slice) == 0 {
 		return 0
 	}
-
 	nonZero := 0
 	for _, str := range *slice {
 		if str != "" {
@@ -17,24 +14,5 @@ func Compact(slice *[]string) int {
 	}
 	// Resize the slice to only keep non-zero elements
 	*slice = (*slice)[:nonZero]
-
 	return nonZero
-}
-
-func main() {
-	s := []string{"a", "", "b", "", "c"}
-
-	// Print original slice
-	for _, v := range s {
-		if v != "" {
-			fmt.Println(v)
-		}
-	}
-
-	count := Compact(&s)
-
-	fmt.Println("Size after compacting:", count)
-	for _, v := range s {
-		fmt.Println(v)
-	}
 }
