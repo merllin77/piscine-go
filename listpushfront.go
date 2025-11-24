d1e4e52996d0b02898733746ecb2097b14ee1da9
ktallaris <netcitizen@email.com>
2025-11-24 15:10:20 +0200

    updated

--- listpushfront.go
+++ listpushfront.go
@@ -1,14 +1,14 @@
 package student
 
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
+// type NodeL struct {
+// 	Data interface{}
+// 	Next *NodeL
+// }
 
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
+// type List struct {
+// 	Head *NodeL
+// 	Tail *NodeL
+// }
 
 func ListPushFront(l *List, data interface{}) {
 	newNode := &NodeL{Data: data}
