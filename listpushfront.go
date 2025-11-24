75e6ff509a4c2aaa268e6e94148711d953a3485e
ktallaris <netcitizen@email.com>
2025-11-24 15:09:25 +0200

    upload

---
+++ listpushfront.go
@@ -0,0 +1,37 @@
+package student
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushFront(l *List, data interface{}) {
+	newNode := &NodeL{Data: data}
+	if l.Head == nil {
+		l.Head = newNode
+	} else {
+		newNode.Next = l.Head
+		l.Head = newNode
+	}
+	l.Tail = newNode
+}
+
+// func main() {
+// 	link := &List{}
+
+// 	ListPushFront(link, "Hello")
+// 	ListPushFront(link, "man")
+// 	ListPushFront(link, "how are you")
+
+// 	it := link.Head
+// 	for it != nil {
+// 		fmt.Print(it.Data, " ")
+// 		it = it.Next
+// 	}
+// 	fmt.Println()
+// }
