7b08b97657bb22623a2e773bfe8ed54e0ecc0480
ktallaris <netcitizen@email.com>
2025-11-25 13:30:34 +0200

    upload

---
+++ listsize.go
@@ -0,0 +1,45 @@
+package main
+
+// import "fmt"
+
+// type NodeL struct {
+// 	Data interface{}
+// 	Next *NodeL
+// }
+
+// type List struct {
+// 	Head *NodeL
+// 	Tail *NodeL
+// }
+
+// func ListPushFront(l *List, data interface{}) {
+// 	newNode := &NodeL{Data: data}
+// 	if l.Head == nil {
+// 		l.Head = newNode
+// 	} else {
+// 		newNode.Next = l.Head
+// 		l.Head = newNode
+// 	}
+// 	l.Tail = newNode
+// }
+
+func ListSize(l *List) int {
+	cnt := 0
+	currentnode := l.Head
+	for currentnode != nil {
+		cnt++
+		currentnode = currentnode.Next
+	}
+	return cnt
+}
+
+// func main() {
+// 	link := &List{}
+
+// 	ListPushFront(link, "Hello")
+// 	ListPushFront(link, "2")
+// 	ListPushFront(link, "you")
+// 	ListPushFront(link, "man")
+
+// 	fmt.Println(ListSize(link))
+// }
