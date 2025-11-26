842caf7916f1f762d75a12742a9c1ea5e9ea5ff1
Konstantinos Tallaris <ktallaris@noreply.platform.zone01.gr>
2025-11-26 00:59:47 +0000

    Update listat.go

--- listat.go
+++ listat.go
@@ -1,15 +1,16 @@
 package student
 
-func ListAt(l *NodeL, pos int) *NodeL{
-    if l==nil || pos <0{
-        return nil
-    }
-    current:=l
-    for i:=0; i<pos; i++ {
-        if current.Next == nil {
-            return nil
-        }
-        current=current.Next
-    }
-    return current
-}
+func ListAt(l *NodeL, pos int) *NodeL {
+	if l == nil || pos < 0 {
+		return nil
+	}
+	current := l
+	for i := 0; i < pos; i++ {
+		if current.Next == nil {
+			return nil
+		}
+		current = current.Next
+	}
+	return current
+ }
+ 
